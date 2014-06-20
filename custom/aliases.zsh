ea6d917ebabd23367dd5a4ff350ce90de6e78fb6
areteix <areteix@gmail.com>
2014-06-20 11:47:14 +0530

    added zsh-autosuggestions and zsh-syntax-highlighting to custom

---
+++ custom/aliases.zsh
@@ -0,0 +1,18 @@
+alias -s tex=vim
+alias -s py=vim
+alias -s ml=vim
+alias -s c=vim
+alias -s cpp=vim
+alias -s html=vim
+alias -s zsh=vim
+alias -s java=vim
+alias -s log=vim
+alias -s pdf="evince 2>/dev/null"
+alias -s mp4="smplayer 2>/dev/null"
+alias -s avi="smplayer 2>/dev/null"
+alias -s mkv="smplayer 2>/dev/null"
+alias -s mpeg="smplayer 2>/dev/null"
+alias -s jpg=feh
+alias -s png=feh
+
+alias code="cd $HOME/workspace/"
