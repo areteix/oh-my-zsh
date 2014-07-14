09726bf2f69cfd0fd8d99ea4d2b408e9a505a3a2
areteix <areteix@gmail.com>
2014-07-14 14:36:47 -0400

    brightness control

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -6,14 +6,14 @@ alias -s cpp=vim
 alias -s html=vim
 alias -s java=vim
 alias -s log=vim
-alias -s pdf="evince 2>/dev/null"
+alias -s pdf="zathura 2>/dev/null"
 alias -s mp4="smplayer 2>/dev/null"
 alias -s avi="smplayer 2>/dev/null"
 alias -s mkv="smplayer 2>/dev/null"
 alias -s mpeg="smplayer 2>/dev/null"
 alias -s jpg=feh
 alias -s png=feh
-
+alias acroread="acroread 2>/dev/null"
 alias code="cd $HOME/workspace/; ls"
 
 alias gn="geeknote"
@@ -23,3 +23,7 @@ alias gn="geeknote"
 alias gsync="cd /media/data/googledrive && grive"
 alias sshint="ssh root@192.168.1.2"
 alias sshof="ssh root@192.168.1.1"
+
+alias pinggoog="ping -c 4 google.com"
+alias upbright="xbacklight -inc 10"
+alias downbright="xbacklight -dec 10"
